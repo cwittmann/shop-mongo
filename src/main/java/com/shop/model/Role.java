8c3a01eabfdfc399dd160ef0063a60aca2c613ce
cwittmann <christofwittmann@web.de>
2020-06-01 15:28:49 +0200

    feat: role with nested rights

--- src/main/java/com/shop/model/Role.java
+++ src/main/java/com/shop/model/Role.java
@@ -6,9 +6,11 @@ public class Role {
     @Id
     public String id;
     public String name;
+    public Right[] rights;
 
-    public Role(String id, String name) {
+    public Role(String id, String name, Right[] rights) {
         this.id = id;
         this.name = name;
+        this.rights = rights;
     }
 }
