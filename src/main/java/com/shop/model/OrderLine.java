52e01fe1d4d91fc2aa2d68aac8cfd9e2d6bf64ee
cwittmann <christofwittmann@web.de>
2020-06-01 19:53:05 +0200

    fix: orderLine with productId

--- src/main/java/com/shop/model/OrderLine.java
+++ src/main/java/com/shop/model/OrderLine.java
@@ -8,14 +8,14 @@ public class OrderLine {
     public String id;
     public String orderId;
     public Integer amount;
-    public String productid;
+    public String productId;
     public Product product;
 
     public OrderLine(String id, String orderId, Integer amount, String productId, Product product) {
         this.id = id;
         this.orderId = orderId;
         this.amount = amount;
-        this.productid = productId;
+        this.productId = productId;
         this.product = product;
     }
 }
